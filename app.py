129f0c36ac213cbfc8a856a24d8481663b5f1bb5
mysgrown <76728138+0gaurav4@users.noreply.github.com>
2024-12-16 10:34:51 +0530

    Add files via upload
    v2

--- app.py
+++ app.py
@@ -16,10 +16,15 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    df = pd.read_csv('WorkData.csv')
+    data = df.to_html(classes='table table-striped')
+    return render_template('about.html', data=data)
+
+import pandas as pd
 
 @app.route('/analysis', methods=['GET','POST'])
 def analysis():
+    df = pd.read_csv('WorkData.csv')  # Add the correct path to your CSV file
     if request.method=='POST':
         age = request.form.get('age')
         inc = request.form.get('inc')
@@ -29,16 +34,21 @@ def analysis():
         pred=load_model.predict(inp)[0]
         #print(pred)
         if pred==0:
-            output='Customers with low income & low expenditure'
+            output=f'Customers belongs the group {pred} : [with low income & low expenditure]'
+            output1=f"Number of customers in group {pred} are : {df[df.group == 0].shape[0]}"
         elif pred==1:
-            output='Customers with high income & high expenditure'
+            output=f'Customers belongs the group {pred} : [with high income & high expenditure]'
+            output1=f"Number of customers in group {pred} are : {df[df.group == 1].shape[0]}"
         elif pred==2:
-            output='Customers with medium income & medium expenditure'
+            output=f'Customers belongs the group {pred} : [with medium income & medium expenditure]'
+            output1=f"Number of customers in group {pred} are : {df[df.group == 2].shape[0]}"
         elif pred==3:
-            output='Customers with high income & low expenditure'
+            output=f'Customers belongs the group {pred} : [with high income & low expenditure]'
+            output1=f"Number of customers in group {pred} are : {df[df.group == 3].shape[0]}"
         elif pred==4:
-            output='Customers with low income & high expenditure'
-        return render_template('analysis.html',output=output)
+            output=f'Customers belongs the group {pred} : [with low income & medium expenditure]'
+            output1=f"Number of customers in group {pred} are : {df[df.group == 4].shape[0]}"
+        return render_template('analysis.html',output=output, output1=output1)
     return render_template('analysis.html')
 
 @app.route('/results', methods=['GET','POST'])
